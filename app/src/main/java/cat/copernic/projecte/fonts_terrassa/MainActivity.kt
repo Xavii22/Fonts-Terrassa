32b290d0e2376a60b01c61df66508e4bd899daec
Roger Puig <puig.font.roger@alumnat.copernic.cat>
2021-11-04 13:15:14 +0100

    Base Navigation Completed

--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/MainActivity.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/MainActivity.kt
@@ -14,26 +14,13 @@ import cat.copernic.projecte.fonts_terrassa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        setContentView(binding.root)
 
-
-        /*binding.navBar.setOnItemSelectedListener { id ->
-            when(id){
-                R.id.fragment_list -> ListFragment()
-                R.id.fragment_map -> MapFragment()
-                R.id.fragment_info -> InfoFragment()
-            }
-        }*/
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController, drawerLayout)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 }
