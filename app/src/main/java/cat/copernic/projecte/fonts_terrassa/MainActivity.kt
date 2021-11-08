ca934efa08c503a028d99366d4a5e2d0d43491df
Roger Puig <puig.font.roger@alumnat.copernic.cat>
2021-11-08 10:29:58 +0100

    More navigation options and screens with the list

--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/MainActivity.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/MainActivity.kt
@@ -1,13 +1,8 @@
 package cat.copernic.projecte.fonts_terrassa
 
 import android.os.Bundle
-import android.text.format.DateFormat
-import android.util.Log
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import cat.copernic.projecte.fonts_terrassa.databinding.ActivityMainBinding
--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/MapFragment.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/MapFragment.kt
@@ -2,6 +2,7 @@
 package cat.copernic.projecte.fonts_terrassa
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,12 +14,15 @@ import cat.copernic.projecte.fonts_terrassa.databinding.FragmentMapBinding
 class MapFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+
         val binding: FragmentMapBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_map, container, false)
 
         binding.button3.setOnClickListener {
-            findNavController().navigate(MapFragmentDirections.actionFragmentMapToFragmentList())
+            findNavController().navigate(MapFragmentDirections.actionFragmentMapToViewFontFragment2())
         }
+
+
         return binding.root
     }
 }
