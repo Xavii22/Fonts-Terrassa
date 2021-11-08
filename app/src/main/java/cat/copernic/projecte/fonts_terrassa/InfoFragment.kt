b8c26483b38d78326def1f834cae54e7261951b3
Xavi <xaviercarneplandolit@gmail.com>
2021-11-08 10:30:28 +0100

    Navigation info and login

--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/InfoFragment.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/InfoFragment.kt
@@ -1,3 +1,4 @@
+
 package cat.copernic.projecte.fonts_terrassa
 
 import android.os.Bundle
@@ -5,15 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import cat.copernic.projecte.fonts_terrassa.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding: FragmentInfoBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_map, container, false)
 
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding.btnBeure.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoGosFragment())
+        }
+        binding.btnBeureSingular.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoGosFragment())
+        }
+        binding.btnOrnamental.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoGosFragment())
+        }
+        binding.btnNatural.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoGosFragment())
+        }
+
+        binding.btnGos.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoGosFragment())
+        }
+        return binding.root
     }
 }
