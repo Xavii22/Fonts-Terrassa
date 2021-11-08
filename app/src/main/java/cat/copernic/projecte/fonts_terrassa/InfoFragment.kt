e5326b5b7e3ebb3d66d34ddbbe477157b3f2efb4
Xavi <xaviercarneplandolit@gmail.com>
2021-11-08 11:43:18 +0100

    Info screen completed

--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/InfoFragment.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/InfoFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import cat.copernic.projecte.fonts_terrassa.databinding.FragmentInfoBinding
 
 class InfoFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val binding: FragmentInfoBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_info, container, false)
 
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding.btnBeure.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoBeureFragment())
+        }
+        binding.btnSingular.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoSingularFragment())
+        }
+        binding.btnOrnamental.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoOrnamentalFragment())
+        }
+        binding.btnNatural.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoNaturalFragment())
+        }
+
+        binding.btnGos.setOnClickListener {
+            findNavController().navigate(InfoFragmentDirections.actionFragmentInfoToInfoGosFragment())
+        }
+        return binding.root
     }
 }
