c4bb22738d6b2bf07be225139804231d0166a717
Roger Puig <puig.font.roger@alumnat.copernic.cat>
2021-11-22 09:45:55 +0100

    ListFragment.kt data from DB

--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/ListFragment.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/ListFragment.kt
@@ -1,6 +1,7 @@
 package cat.copernic.projecte.fonts_terrassa
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +18,7 @@ class ListFragment : Fragment() {
     private lateinit var binding: FragmentListBinding
     private val myAdapter: FontRecyclerAdapter = FontRecyclerAdapter()
     private val db = FirebaseFirestore.getInstance()
+    private var fonts: ArrayList<Font> = arrayListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -24,34 +26,24 @@ class ListFragment : Fragment() {
        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_list, container, false)
 
-        setUpRecyclerView()
+
+
+        db.collection("fonts").whereEqualTo("type", 1)
+            .get()
+            .addOnSuccessListener{ documents ->
+                for (document in documents){
+                    fonts.add(Font(
+                        document.get("name").toString(),
+                        document.get("lat").toString().toDouble(),
+                        document.get("lon").toString().toDouble(),
+                        1))
+                }
+                binding.rvFonts.setHasFixedSize(true)
+                binding.rvFonts.layoutManager = LinearLayoutManager(context)
+                context?.let { myAdapter.fontsRecyclerAdapter(fonts, it) }
+                binding.rvFonts.adapter = myAdapter
+            }
 
         return binding.root
-    }
-
-    private fun setUpRecyclerView(){
-        //Especifiquem que els fills del RV seran del mateix tamany i així optimitzem la seva creació
-        binding.rvFonts.setHasFixedSize(true)
-
-        //indiquem que el RV es mostrarà en format llista
-        binding.rvFonts.layoutManager = LinearLayoutManager(context)
-
-        //generem el adapter
-        context?.let { myAdapter.fontsRecyclerAdapter(getFonts(), it) }
-
-        //assignem el adapter al RV
-        binding.rvFonts.adapter = myAdapter
-    }
-
-    private fun getFonts(): MutableList<Font> {
-        val fonts: MutableList<Font> = arrayListOf()
-        db.collection("fonts").whereEqualTo("type", 1).get().addOnSuccessListener{ documents ->
-            for (document in documents) {
-                fonts.add(Font(document.get("name").toString(), document.get("lat").toString().toDouble(),
-                    document.get("lon").toString().toDouble(), 1))
-            }
-        }
-
-        return fonts
     }
 }
