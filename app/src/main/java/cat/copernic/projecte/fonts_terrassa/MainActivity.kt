f14cc2dbf9af3b5047f7fa4f5fd0a2719bdb656d
Roger Puig <puig.font.roger@alumnat.copernic.cat>
2021-11-24 10:29:24 +0100

    Light mode only

--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/MainActivity.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/MainActivity.kt
@@ -2,6 +2,7 @@ package cat.copernic.projecte.fonts_terrassa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
@@ -13,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
--- app/src/main/java/cat/copernic/projecte/fonts_terrassa/SplashScreenActivity.kt
+++ app/src/main/java/cat/copernic/projecte/fonts_terrassa/SplashScreenActivity.kt
@@ -3,10 +3,12 @@ package cat.copernic.projecte.fonts_terrassa
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
