13898c19743163f2c097b417f4f248470aa284e2
Xavi <xaviercarneplandolit@gmail.com>
2022-01-22 15:47:58 +0100

    evaluate button changed

---
+++ app/src/test/java/cat/copernic/projecte/fonts_terrassa/FontsSizeTest.kt
@@ -0,0 +1,20 @@
+package cat.copernic.projecte.fonts_terrassa
+
+import cat.copernic.projecte.fonts_terrassa.adapters.FontRecyclerAdapter
+import cat.copernic.projecte.fonts_terrassa.models.Font
+import org.junit.Assert
+import org.junit.Test
+/*
+class FontsSizeTest {
+    val font = Font("001", "nomprova", 41.0, 2.0, "inffoo", 3, "noy,34", 0.4)
+
+    @Test
+    fun testCaracterEspecials(){
+
+        var fonts: ArrayList<Font> = arrayListOf()
+        fonts.add(font)
+        val util = FontRecyclerAdapter(fonts, context)
+        val paraula2 = util.noCaractersEspecials("Matemàtiques")
+        Assert.assertEquals(paraula, paraula2)
+    }
+}*/
